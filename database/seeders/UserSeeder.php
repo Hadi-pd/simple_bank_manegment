cce02feb4afd71ed2f620f693ab19efa2d644758
Hadi-pd <h7ad3i@chmail.ir>
2021-12-01 16:30:10 +0330

    Add seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Jetstream\Rules\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('admin')
+            ]
+        );
+        $role = Role::create(
+            ['name' => 'admin']
+        );
+        RoleUser::create(
+            [
+                'role_id' => $role->id,
+                'user_id' => $user->id
+            ]
+        );
+        $user = User::updateOrCreate(
+            ['email' => 'user@gmail.com'],
+            [
+                'name' => 'user1',
+                'password' => Hash::make('user')
+            ]
+        );
+        $role = Role::create(
+            ['name' => 'user']
+        );
+        RoleUser::create(
+            [
+                'role_id' => $role->id,
+                'user_id' => $user->id
+            ]
+        );
+    }
+}
