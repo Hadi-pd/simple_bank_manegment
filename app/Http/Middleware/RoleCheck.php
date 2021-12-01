51e772ec379f0b90891f41df44b47e6959255a5e
Hadi-pd <h7ad3i@chmail.ir>
2021-12-01 19:53:33 +0330

    Add RoleChecke Middleware

---
+++ app/Http/Middleware/RoleCheck.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoleCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check()){
+            if(Auth::user()->isAdmin()){
+                return $next($request);
+            }
+            return redirect()->back();
+        }
+    }
+}
