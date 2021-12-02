d58aba30c0b334646a58657019587e566abff966
Hadi-pd <h7ad3i@chmail.ir>
2021-12-02 15:54:33 +0330

    Add admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +10,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth', 'isAdmin'])->group( function () {
+    Route::get('admin',[AdminController::class,'index']);
+});
