5146be7ac178233dd69d2ee83bf5580564d00c4b
Hadi-pd <h7ad3i@chmail.ir>
2021-12-04 18:44:41 +0330

    Make Deposets Model

---
+++ database/migrations/2021_12_04_133812_create_deposits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepositsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->string('deposit_type')->nullable();
+            $table->unsignedBigInteger('loan_id')->defaultValue(0);
+            $table->unsignedBigInteger('tracking_code')->nullable();
+            $table->timestamp('deposit_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deposits');
+    }
+}
