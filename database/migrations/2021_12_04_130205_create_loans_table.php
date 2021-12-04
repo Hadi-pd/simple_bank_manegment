a5ba0b9b2a3eac632f095881e2516e9121d49272
Hadi-pd <h7ad3i@chmail.ir>
2021-12-04 18:45:04 +0330

    Make Loans Model

---
+++ database/migrations/2021_12_04_130205_create_loans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('l_guarantor1_id');
+            $table->unsignedBigInteger('l_guarantor2_id');
+            $table->unsignedBigInteger('l_amount');
+            $table->unsignedBigInteger('l_percentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
