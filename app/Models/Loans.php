f7e524ccc2262afc4b48f9e4264e065957cb8be5
Hadi-pd <h7ad3i@chmail.ir>
2021-12-06 14:05:41 +0330

    add loans relationship

--- app/Models/Loans.php
+++ app/Models/Loans.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loans extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    protected $table = 'loans';
+    protected $fillable=[
+       'account_id',
+       'l_guarantor1_id',
+       'l_guarantor2_id',
+       'l_amount',
+       'l_percentage'
+     ];
+
+    public function Accounts(){
+        return $this->hasOne(Accounts::class, 'id');
+    }
 }
